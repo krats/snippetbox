ad26981858ba24ac4b6b86054b508ca1bfa8ef2c
Karthik Bashetty <karthiksathy@gmail.com>
2024-06-19 10:56:40 +0530

    A basic router with multiple views

--- main.go
+++ main.go
@@ -7,12 +7,31 @@ import (
 
 func home(w http.ResponseWriter, _ *http.Request) {
 	_, err := w.Write([]byte("Hello from Snippetbox!"))
-	log.Fatal(err)
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func snippetView(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("Snippet view!"))
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+func snippetCreate(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("Snippet create!"))
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Print("Listening on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
